286be393e0d48d172cb6e5711b6d191ca7388305
Libin Mathew <libinmath3w@users.noreply.github.com>
2023-03-17 17:24:50 +0530

    Update app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,19 +2,20 @@ let express = require('express');
 let app = express();
 
 app.get('/', function (req, res) {
-  let obj = {
-    endpoints: [
-      "/ping",
-      "/current-date",
-      "/fibo/:n",
-      "Hello World \n",
-    ]
-  };
-  res.send(obj);
+  // let obj = {
+  //   endpoints: [
+  //     "/ping",
+  //     "/current-date",
+  //     "/fibo/:n",
+  //     "Hello World \n",
+  //   ]
+  // };
+  res.render('index', { title: 'My Node.js App' })
+  res.send('<h1>Hello, world! </br> Stunning Broccoli Node JS App For Production</br> /ping </br> /current-date </br> <p><a href="/about">About Us</a></p> </h1>');
 });
 
 app.get('/ping', function (req, res) {
-  res.send("pong");
+  res.send("<h1>pong</h1>");
 });
 
 
@@ -25,6 +26,14 @@ app.get('/current-date', function (req, res) {
   };
   res.send(obj);
 });
+
+app.get('/about', (req, res) => {
+  res.send('<h1>About Us</h1><p>We are a company that specializes in creating Node.js apps.</p><p><a href="/">Home</a></p>')
+})
+
+app.get('/contact', (req, res) => {
+  res.send('<h1>Contact Us</h1><p>You can contact us at contact@example.com.</p>')
+})
 
 app.get('/fibo/:n', function (req, res) {
   let obj = {
