6d9948a52a85ca44f5b1bd655d996b63b5fe04e9
Libin Mathew <libinmath3w@users.noreply.github.com>
2023-03-17 18:53:27 +0530

    updated with some new data

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,14 +2,7 @@ let express = require('express');
 let app = express();
 
 app.get('/', function (req, res) {
-  let obj = {
-    endpoints: [
-      "/ping",
-      "/current-date",
-      "/fibo/:n",
-      "Hello World \n",
-    ]
-  };
+  let obj = '<h1>Hello, world! </br> Stunning Broccoli Node JS App For Production</br> /ping </br> /current-date </br> <p><a href="/about">About Us</a></p> </h1>';
   res.send(obj);
 });
 
@@ -17,6 +10,13 @@ app.get('/ping', function (req, res) {
   res.send("pong");
 });
 
+app.get('/about', (req, res) => {
+  res.send('<h1>About Us</h1><p>We are a company that specializes in creating Node.js apps.</p><p><a href="/">Home</a></p>')
+});
+
+app.get('/contact', (req, res) => {
+  res.send('<h1>Contact Us</h1><p>You can contact us at contact@example.com.</p>')
+});
 
 app.get('/current-date', function (req, res) {
   let obj = {
@@ -39,6 +39,11 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
+res.status(err.status || 500);
+res.json({
+  message: err.message,
+  error: err
+});
 
 function fibo(n) { // 1
   if (n < 2)
