bfec4d19a69f9616796a7b77b7ed8e63e3e6b680
Libin Mathew <libinmath3w@users.noreply.github.com>
2023-03-17 12:30:18 +0530

    app updated to new version

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
-FROM python:3.7-alpine
-WORKDIR /code
-ENV FLASK_APP=app.py
-ENV FLASK_RUN_HOST=0.0.0.0
-RUN apk add --no-cache gcc musl-dev linux-headers
-COPY requirements.txt requirements.txt
-RUN pip install -r requirements.txt
-RUN apk add --no-cache curl
-EXPOSE 5000
-EXPOSE 8001
+# We will use python:3.10-alpine as the base image for building the Flask container
+FROM python:3.10-alpine
+# It specifies the working directory where the Docker container will run
+WORKDIR /app
+# Copying all the application files to the working directory
 COPY . .
-CMD ["flask", "run"]
+# Install all the dependencies required to run the Flask application
+RUN pip install -r requirements.txt
+# Expose the Docker container for the application to run on port 5000
+EXPOSE 5000
+# The command required to run the Dockerized application
+CMD ["python", "/app/app.py"]
--- app.py
+++ app.py
@@ -1,31 +1,10 @@
-from flask import Flask, json
+from flask import Flask
 import socket
-from prometheus_client import Histogram, Counter, Gauge, start_http_server
-import prometheus_client
-
 
 app = Flask(__name__)
-
-REQUESTS = Counter('mywebapp_requests_total', 'Stunning Broccoli Flask App requested.', labelnames=['path'])
-LATENCY = Histogram('mywebapp_latency_seconds', 'Time for a request', labelnames=['path'])
-start_http_server(8001)
-
-@LATENCY.time()
 @app.route("/")
 def hello_world():
-    REQUESTS.labels("/").inc()
-    html = f"<h1>Stunning Broccoli Flask App - My WebApp!</h1> Hello World! Served from <b>{socket.gethostname()}</b><br> Dev App </br>"
+    html = f"<h1>Stunning Broccoli Flask App - My Flask WebApp!</h1> Hello World! Served from <b>{socket.gethostname()}</b><br> Dev App </br>"
     return html
-
-@LATENCY.time()
-@app.route("/bad")
-def bad_page():
-    REQUESTS.labels("/bad").inc()
-    errorcode = 404
-    data = {'error': errorcode}
-    response = app.response_class(
-        response=json.dumps(data),
-        status=errorcode,
-        mimetype='application/json'
-    )
-    return response
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
